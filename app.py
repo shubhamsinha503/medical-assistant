d28c6e202e51161f3b09cadef4d0baeea1c5b31c
SHUBHAM SINHA <94913494+shubhamsinha503@users.noreply.github.com>
2024-12-17 03:23:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_gemini_response(api_key, prompt, image):
 # Fetch nearby doctors using Google Maps API
 def get_nearby_doctors(location, specialty):
     try:
-        api_key = "AIzaSyBKrIn4ruEluFEywJD3G84qBMHSgUfiCT0"
+        api_key = "google maps"
         url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=doctors+{specialty}+near+{location}&key={api_key}"
 
         response = requests.get(url)
@@ -75,12 +75,18 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Header
+# Header with an image
+#st.image("12.png", use_column_width=False, caption="Welcome to Gemini Vision Bot",width=600)
+left_co, cent_co, last_co = st.columns(3)
+with cent_co:
+    st.image("12.png")
+# Replace with your image path
 st.markdown("<div class='stHeader'>GenAI Medical Application</div>", unsafe_allow_html=True)
 
 # Sidebar for input fields
 st.sidebar.title("Input Details")
-api_key = st.sidebar.text_input("Enter your Gemini passkey: ", type="password")
+st.sidebar.image("122.jpg", use_column_width=True, caption="Input Patient Data")  # Replace with your image path
+api_key = "gemini api key"
 location = st.sidebar.text_input("Enter your location (e.g., Delhi): ")
 uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
@@ -95,9 +101,9 @@ if st.sidebar.button("Generate AI Report"):
     if api_key and image is not None and location:
         try:
             # Generate AI response
-            prompt = "generate an AI report of the detected disease in image like finding, precautions, recommendations, and suggest the kind of doctor to be consulted"
+            prompt = "generate an AI report of the detected disease in image like finding, precautions, recommendations, and suggest the kind of doctor to be consulted and also give some risk graph and give no disclaimer"
             ai_response = get_gemini_response(api_key, prompt, image)
-            st.subheader("AI Report:")
+            st.title("AI Report:")
             st.markdown(f"<div class='stSubheader'>{ai_response}</div>", unsafe_allow_html=True)
 
             # Extract doctor type from the response
@@ -123,12 +129,15 @@ if st.sidebar.button("Generate AI Report"):
     else:
         st.error("Please provide all required inputs.")
 
-# Footer
+# Add a footer image
+#st.image("footer_image.jpg", use_column_width=True, caption="Empowering Medical Decisions with AI")  # Replace with your image path
+
+# Footer text
 st.markdown(
     """
     <hr>
     <div style="text-align: center; font-size: 14px;">
-        Powered by <b>Gemini AI</b> | Developed with ❤️ by Your Name
+        Powered by <b>Gemini AI</b> | Developed with ❤️ 
     </div>
     """,
     unsafe_allow_html=True
